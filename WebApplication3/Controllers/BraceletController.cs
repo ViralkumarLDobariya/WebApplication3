acf235c926cec9e65c2691f10a3249226a187e7b
Viral <Viral@DESKTOP-QBJB296>
2024-06-13 13:52:30 -0400

    Add project files.

---
+++ WebApplication3/Controllers/BraceletController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace WebApplication3.Controllers;
+public class BraceletController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        return View();
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public string Welcome(string name, int ID = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+    }
+}
